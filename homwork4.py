27a1e9cbe5587e3273bea0fd0b9abf8241d033eb
Ekaterina80git <timofeewa.yekat@yandex.ru>
2024-07-03 23:31:37 +0300

    Add files via upload

---
+++ homwork4.py
@@ -0,0 +1,34 @@
+# "1st program"
+a = (9 * 5 )
+print(a)
+print(9**0.5)
+print(9**0.5*5)
+# "2 and program"
+print(9.99 > 9.98 , 1000 != 1001)
+# "3 and program"
+a = 1234
+b = 5678
+g = (1234//10)
+print(g)
+f = (123%100)
+print(f)
+c = (5678//10)
+print(c)
+d =(567%100)
+print(d)
+print((int(f+d)))
+# " 4 th program"
+print(24==24 or 10.24==24.15 and 125>150 or 17==17 and 17.25==25.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
